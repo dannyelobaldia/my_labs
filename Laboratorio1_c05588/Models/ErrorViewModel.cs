2f4465eac12e39bed49dd794b143d9f4ef42d292
Dannyel Obaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-03-18 10:05:28 -0600

    Creación del laboratorio1 como proyecto MVC.

---
+++ Laboratorio1_c05588/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_c05588.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
