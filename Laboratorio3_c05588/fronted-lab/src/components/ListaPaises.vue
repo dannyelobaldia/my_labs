1ee711cadb77154a0cf66ad2713e3074aad89605
Dannyel Obaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-04-02 22:39:12 -0600

    Laboratorio 3 completado. Sin code challenge.

---
+++ Laboratorio3_c05588/fronted-lab/src/components/ListaPaises.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Lista de Paises</h1>
+        <table
+            class="table is-bordered is-striped is-narrow is-hoverable
+            is-fullwidth"
+        >
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Continente</th>
+                    <th>Idioma</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(pais, index) of paises" :key="index">
+                    <td>{{ pais.nombre }}</td>
+                    <td>{{ pais.continente }}</td>
+                    <td>{{ pais.idioma }}</td>
+                    <td>
+                        <button class="btn btn-secondary btn-sm">Editar</button>
+                        <button class="btn btn-danger btn-sm">Eliminar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ListaPaises',
+        data() {
+            return {
+                paises: [
+                    { nombre: 'Costa Rica', continente: 'América', idioma: 'Español' },
+                    { nombre: 'Japón', continente: 'Asia', idioma: 'Japonés' },
+                    { nombre: 'Corea del Sur', continente: 'Asia', idioma: 'Coreano' },
+                    { nombre: 'Italia', continente: 'Europa', idioma: 'Italiano' },
+                    { nombre: 'Alemania', continente: 'Europa', idioma: 'Alemán' },
+                ],
+            };
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
