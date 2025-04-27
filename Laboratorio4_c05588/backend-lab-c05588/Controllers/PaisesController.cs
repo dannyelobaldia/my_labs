111a29b66be42402c3eaf50edc66259ef4a676f4
dannyelobaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-04-27 14:26:41 -0600

    Laboratorio #5. Modificar laboratorio 4 para manejar agregar paises a la base de datos

--- Laboratorio4_c05588/backend-lab-c05588/Controllers/PaisesController.cs
+++ Laboratorio4_c05588/backend-lab-c05588/Controllers/PaisesController.cs
@@ -22,5 +22,26 @@ namespace backend_lab_c05588.Controllers
             var paises = _paisesHandler.ObtenerPaises();
             return paises;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> CrearPais(PaisModel pais)
+        {
+            try
+            {
+                if (pais == null)
+                {
+                    return BadRequest();
+                }
+
+                PaisesHandler paisesHandler = new PaisesHandler();
+                var resultado = paisesHandler.CrearPais(pais);
+                return new JsonResult(resultado);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error creando país");
+            }
+        }
     }
 }
